d8c4b20e5c1cbb46d7d9330b97a3c02806143c1d
Kiran Shams <160856436+kiranShamsHere@users.noreply.github.com>
2024-03-20 02:24:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+function a() {
+  a = new Audio("a1.mp3");
+  a.play();
+}
+function b() {
+  a = new Audio("b1.mp3");
+  a.play();
+}
+function c() {
+  a = new Audio("c1.mp3");
+  a.play();
+}
+function d() {
+  a = new Audio("d1.mp3");
+  a.play();
+}
+function e() {
+  a = new Audio("e1.mp3");
+  a.play();
+}
+function f() {
+  a = new Audio("f.mp3");
+  a.play();
+}
+function g() {
+  a = new Audio("g.mp3");
+  a.play();
+}
+function h() {
+  a = new Audio("h.mp3");
+  a.play();
+}
+function i() {
+  a = new Audio("i.mp3");
+  a.play();
+}
+function j() {
+  a = new Audio("j.mp3");
+  a.play();
+}
+function k() {
+  a = new Audio("k.mp3");
+  a.play();
+}
+function l() {
+  a = new Audio("l.mp3");
+  a.play();
+}
+function m() {
+  a = new Audio("m.mp3");
+  a.play();
+}
+function n() {
+  a = new Audio("n.mp3");
+  a.play();
+}
+function o() {
+  a = new Audio("o.mp3");
+  a.play();
+}
+function p() {
+  a = new Audio("p.mp3");
+  a.play();
+}
+function q() {
+  a = new Audio("q.mp3");
+  a.play();
+}
+function r() {
+  a = new Audio("r.mp3");
+  a.play();
+}
+function s() {
+  a = new Audio("s.mp3");
+  a.play();
+}
+function t() {
+  a = new Audio("t.mp3");
+  a.play();
+}
+function u() {
+  a = new Audio("u.mp3");
+  a.play();
+}
+function v() {
+  a = new Audio("v.mp3");
+  a.play();
+}
+function w() {
+  a = new Audio("w.mp3");
+  a.play();
+}
+function x() {
+  a = new Audio("x.mp3");
+  a.play();
+}
+function y() {
+  a = new Audio("y.mp3");
+  a.play();
+}
+function z() {
+  a = new Audio("z.mp3");
+  a.play();
+}
